09505e9f3b5dfa63a238e15fafe715c1107a5047
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-01-11 16:14:49 +0000

    build angular code

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/main",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["ts-loader", "angular2-template-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["raw-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
